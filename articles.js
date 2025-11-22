a5d1e6e4e9f2a25a94e869a357cde8a7cdef30c1
kiendey24 <kiendey24@gmail.com>
2025-11-22 14:22:39 +0700

    Create articles.js

---
+++ articles.js
@@ -0,0 +1,14 @@
+{
+  id: "1",
+  slug: "1-nu-sinh-dung-non-bao-hiem-tuong-tac-lien-tuc-vao-1-nam-sinh",
+  title: "1 nữ sinh dùng nón bảo hiểm tương tác liên tục vào 1 nam sinh",
+  date: "22/11/2025",
+  category: "Pháp luật",
+  summary: "‼️Trường THPT Đào Sơn Tây - TP.HCM: 1 nữ sinh dùng nón bảo hiểm tương tác liên tục vào 1 nam sinh.",
+  mediaEmbeds: [
+    `<div style="position:relative;padding-bottom:56.25%;height:0;overflow:hidden;"><iframe src="https://geo.dailymotion.com/player.html?video=x9u8ioe"style="width:100%; height:100%; position:absolute; left:0px; top:0px; overflow:hidden; border:none;"allowfullscreentitle="Dailymotion Video Player"allow="web-share"></iframe></div>`
+  ],
+  content: `
+      <p>1 nữ sinh dùng nón bảo hiểm tương tác liên tục vào 1 nam sinh</p>
+  `
+}
